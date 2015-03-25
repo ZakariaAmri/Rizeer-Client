41af823d053d52a63a701392ae05b53590d5fa56
zakhome <zak.amri@gmail.com>
2015-03-25 20:12:37 +0100

    Project Xcode for client

---
+++ Rizeer Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rizeer Client
+//
+//  Created by Youssef Amri on 13/03/2015.
+//  Copyright (c) 2015 Youssef Amri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Rizeer Client/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Rizeer Client
+//
+//  Created by Youssef Amri on 13/03/2015.
+//  Copyright (c) 2015 Youssef Amri. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
